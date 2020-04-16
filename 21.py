8906250cc7304d09be1b12062423ae27a270a10d
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:21:40 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,6 @@
+#21.Write a python program to access elements of list.
+l=[1,2,3,"abc","x","y",5.5,8.9]
+print(l[0])
+print(l[3])
+print(l[4])                                     #accessing elements of list
+print(l[7])
