af8e0cd65633d54d04cd9cd67809d728ba9f981e
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:27:50 +0530

    Create 45.py

---
+++ 45.py
@@ -0,0 +1,14 @@
+#45.Write a python function to find fibonacci series upto to n terms.
+def fibonacci(num,a,b):
+    for i in range(0,num,1):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+
+num=int(input("Enter the number "))
+a=0
+b=1
+print(a,end=" ")
+print(b,end=" ")
+fibonacci(num,a,b)
