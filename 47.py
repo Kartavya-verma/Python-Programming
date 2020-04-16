22fe3770f653c93c1b244d687b56b616e8fcac54
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:28:13 +0530

    Create 47.py

---
+++ 47.py
@@ -0,0 +1,8 @@
+  
+#47.Write a python function to generate a table of any number entered by user.
+def table(num):
+    for i in range(1,11,1):
+        print(num ,"*", i,"=",num*i )
+    
+num=int(input("Enter the number "))
+table(num)
