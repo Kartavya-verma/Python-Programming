9679216d545d63f77aeeb1e9f9aa2012c8680a37
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:17:48 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,7 @@
+#8.Write a python program to accept two integers and check wheather they are equal or not.
+num1=int(input("Enter the first number "))
+num2=int(input("Enter the second number "))
+if num1==num2:
+    print("EQUAL")
+else:
+    print("UNEQUAL")
