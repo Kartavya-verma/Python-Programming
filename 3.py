60525b7b421f0435461f14841d9cdf2f394e70b7
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:16:10 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,17 @@
-class Rectangle:
-    def compute_area(self):
-        length=int(input("Enter the length of the rectangle\n"))
-        width=int(input("Enter the width of the rectangle\n"))
-        return length*width
-
-
-obj=Rectangle()
-area=obj.compute_area()
-print("Area of the Rectangle is: ",area)        
-        
-    
+#3.Write a python program to find percentage of a student.
+print("Marks obtained by student in each subject out of 100:")
+total_marks=0
+hindi=90
+english=80
+maths=70
+social_science=60
+science=50
+print("Hindi:90")
+print("English:80")
+print("Maths:70")
+print("Social Science:60")
+print("Science:50")
+total_marks+=hindi+english+maths+social_science+science
+print("Total marks obtained by student out of 500:\n",total_marks)
+per=total_marks*(100/500)
+print("Percentage:",per)
