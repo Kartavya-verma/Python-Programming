ad6e9b8924bc9ebb6dea71f55f117cab9336134d
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:26:55 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,6 @@
+#40.Write a python program to check whether a given key already exist in a dictionary.
+d={'name':'john','fav_game':'cricket','age':20,'fav_player':'AB de'}
+if 'fav_game' in d:
+    print("PRESENT")
+else:
+    print("NOT PRESENT")
