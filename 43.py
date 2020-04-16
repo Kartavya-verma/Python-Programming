8d34dffcdb9326c38ea06e623509aacc1c93fe8b
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-04-16 21:27:29 +0530

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,6 @@
+#43.Write a python function to find average of five numbers passed parameters.
+def find_average(a,b,c,d,e):
+    average=(a+b+c+d+e)/5
+    return average
+
+print(find_average(2,4,6,8,10))
